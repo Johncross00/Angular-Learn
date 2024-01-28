64a52b3277b0d5c196c2d00a4f73c7465e9a127c
johncross00 <johncross.ktm@gmail.com>
2024-01-28 23:12:02 +0000

    test.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
+import { FormsModule } from '@angular/forms';
+import { ListeCategorieComponent } from './liste-categorie/liste-categorie.component';
+import { NouvelleCategorieComponent } from './nouvelle-categorie/nouvelle-categorie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    ListeCategorieComponent,
+    NouvelleCategorieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
